d076126945c33b217c32a5419611b06a64241575
nihin <mr1809@srmist.edu.in>
2025-02-02 16:29:16 +0530

    Update footer to reflect author name and add LinkedIn profile link

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,13 @@ export function Footer() {
       <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by EmojiExplorer. The source code is available on{" "}
+            Built by NITHIN REDDY MARTHALA. The source code is available on{" "}
             <Link href="https://github.com/Nithinreddy3093/emoji-explorer" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
               GitHub
+            </Link>
+            . Visit my LinkedIn profile{" "}
+            <Link href="https://www.linkedin.com/in/nithin-marthala/" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
+              here
             </Link>
             .
           </p>
